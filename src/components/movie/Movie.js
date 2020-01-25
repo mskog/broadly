2737b4d593ac49fd9db14207e7f85324b0c70890
Magnus Skog <magnus.m.skog@gmail.com>
2020-01-25 15:30:24 +0100

    No download links for downloaded movies

--- src/components/movie/Movie.js
+++ src/components/movie/Movie.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { DateTime } from "luxon";
 
 import { useMovieQuery } from "store/movies";
 
@@ -26,15 +27,18 @@ function Movie(props) {
     overview,
     bestRelease,
     hasKillerRelease,
-    hasAcceptableRelease
+    hasAcceptableRelease,
+    downloadAt
   } = data.movie;
+
+  const downloaded = DateTime.fromISO(downloadAt) <= DateTime.local();
 
   return (
     <div className="pb-20">
       <Top movie={data.movie} />
       <div className="container max-w-2xl px-8 mx-auto">
         <p className="my-4 text-lg text-gray-400">{overview}</p>
-        {bestRelease && (
+        {bestRelease && !downloaded && (
           <div className="mt-10">
             <ReleaseInformation
               acceptable={hasAcceptableRelease}
@@ -43,9 +47,11 @@ function Movie(props) {
             />
           </div>
         )}
-        <div className="flex flex-col pt-4 -mx-2 md:flex-row">
-          <Actions history={history} movie={data.movie} />
-        </div>
+        {!downloaded && (
+          <div className="flex flex-col pt-4 -mx-2 md:flex-row">
+            <Actions history={history} movie={data.movie} />
+          </div>
+        )}
       </div>
     </div>
   );
