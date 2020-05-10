9bebb3431bdb0c2c968e0cb2d135a5c41e7705d7
Magnus Skog <magnus.m.skog@gmail.com>
2020-05-10 15:27:29 +0200

    Nicer display of no results in the search

--- src/components/search/tv_shows/Results.js
+++ src/components/search/tv_shows/Results.js
@@ -13,13 +13,17 @@ export default function Results({ query }) {
   let results = <Loading />;
 
   if (data) {
-    results = data.tvShowSearch.map(result => {
-      return (
-        <div key={result.imdbId} className="px-3 mb-10 md:w-full lg:w-1/2">
-          <Result result={result} />
-        </div>
-      );
-    });
+    if (data.tvShowSearch.length !== 0) {
+      results = data.tvShowSearch.map(result => {
+        return (
+          <div key={result.imdbId} className="px-3 mb-10 md:w-full lg:w-1/2">
+            <Result result={result} />
+          </div>
+        );
+      });
+    } else {
+      results = "Nothing found";
+    }
   }
 
   return <div className="flex-wrap mt-10 -mx-3 md:flex">{results}</div>;
