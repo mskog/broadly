5ad831ae3773c9e45d6e4822f79d75f57077f5cc
Magnus Skog <magnus.m.skog@gmail.com>
2021-05-19 19:18:22 +0200

    Nice release info again

--- src/components/movie/ReleaseInformation.js
+++ src/components/movie/ReleaseInformation.js
@@ -7,15 +7,15 @@ export default function ReleaseInformation({ killer, acceptable, release }) {
 
   const releaseLine = `${source} - ${container} - ${resolutionDisplay(
     resolution
-  )} - ${size}GB`;
+  )} - ${Math.round(size / 1000000000, 2)}GB`;
 
   return (
     <div>
       <h2 className="pb-4 text-3xl text-center text-gray-200 md:text-left">
         Best release
       </h2>
-      <p className="text-lg font-semibold">{releaseName}</p>
-      <p className="mb-4">{releaseLine}</p>
+      <p className="text-lg font-semibold capitalize">{releaseName}</p>
+      <p className="mb-4 capitalize">{releaseLine}</p>
 
       {killer && <h2 className="font-bold">This is a killer release</h2>}
       {!killer && acceptable && (
