928dbd0db863c6a5c2badc4e63e8ad8626386a8a
Magnus Skog <magnus.m.skog@gmail.com>
2021-08-06 16:18:34 +0200

    Fixed yarn

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/styles.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 const rootEl = document.getElementById("root");
 
-const render = Component => {
+const render = (Component) => {
   ReactDOM.render(
     <BrowserRouter>
       <Component />
@@ -21,7 +20,6 @@ render(App);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
 
 if (module.hot) {
   module.hot.accept("./App", () => {
