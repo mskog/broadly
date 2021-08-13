05e4679a16d381251f5c018636ad95c68630b75e
Magnus Skog <magnus.m.skog@gmail.com>
2021-08-13 13:17:42 +0200

    Fix

--- src/components/Calendar/Categories.tsx
+++ src/components/Calendar/Categories.tsx
@@ -6,7 +6,11 @@ const CATEGORIES = ["ALL", "EPISODES", "MOVIES"];
 
 export default function Categories({ category }: { category: string }) {
   const options = CATEGORIES.map((cat) => (
-    <Category key={cat} name={cat} active={category === cat} />
+    <Category
+      key={cat}
+      name={cat}
+      active={category.toLowerCase() === cat.toLowerCase()}
+    />
   ));
 
   return <ul className="flex pt-20">{options}</ul>;
