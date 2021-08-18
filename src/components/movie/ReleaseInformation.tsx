90d07d17b0ab2d57b6cb9a61f037773bfba5e612
Magnus Skog <magnus.m.skog@gmail.com>
2021-08-18 14:06:17 +0200

    Test

---
+++ src/components/movie/ReleaseInformation.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { resolutionDisplay } from "utilities";
+
+type ReleaseInformation = {
+  killer: boolean;
+  acceptable: boolean;
+  release: any;
+};
+
+export default function ReleaseInformation({
+  killer,
+  acceptable,
+  release
+}: ReleaseInformation) {
+  const { releaseName, source, size, resolution, container } = release;
+
+  const releaseLine = `${source} - ${container} - ${resolutionDisplay(
+    resolution
+  )} - ${Math.round(size / 1000000000)}GB`;
+
+  return (
+    <div>
+      <h2 className="pb-4 text-3xl text-center text-gray-200 md:text-left">
+        Best release
+      </h2>
+      <p className="text-lg font-semibold capitalize">{releaseName}</p>
+      <p className="mb-4 capitalize">{releaseLine}</p>
+
+      {killer && <h2 className="font-bold">This is a killer release</h2>}
+      {!killer && acceptable && (
+        <h2 className="font-bold">This is an acceptable release</h2>
+      )}
+    </div>
+  );
+}
